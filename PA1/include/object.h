a26374554be632496176eebb8de04cdb7904ed33
ZeratulAud <Zacharyamcilwain@gmail.com>
2018-09-02 13:10:11 -0700

    Initial Commit

---
+++ PA1/include/object.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    //Object();
+    Object(float mult);
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+    float angleMultiplier;
+    float angle;
+};
+
+#endif /* OBJECT_H */

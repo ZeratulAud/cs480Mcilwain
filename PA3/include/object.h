b0b70f481c04be8b704fc508381162f2fb49ca4f
ZeratulAud <Zacharyamcilwain@gmail.com>
2018-09-18 15:46:33 -0700

    PA3 functionality

---
+++ PA3/include/object.h
@@ -0,0 +1,54 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+#include "window.h"
+
+
+class Object
+{
+  public:
+    //Object();
+    Object();
+    Object(float speed, float scale, float radius);
+    //Object(Object* parentPlanet);
+    ~Object();
+    void Update(unsigned int dt, glm::mat4 orbitOrigin);
+    void Render(GLint& m_modelMatrix);
+
+    void FlipOrbitRotation();
+    void FlipPlanetRotation();
+
+    void ToggleOrbitRotation();
+    void TogglePlanetRotation();
+
+    void AddChild(float speed, float scale, float radius);
+
+    glm::mat4 GetModel();
+
+    float orbitSpeed;
+    float spinSpeed;
+    float planetScale;
+    float orbitRadius;
+
+
+  private:
+
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float orbitAngle;
+	float spinAngle;
+
+    bool stopSpin;
+    bool stopOrbit;
+
+    std::vector<Object*> children;
+};
+
+
+#endif /* OBJECT_H */
